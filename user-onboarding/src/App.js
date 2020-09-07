6e592bb623a128788c5f7a151e837acf9d99b6c1
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-07 18:35:03 -0500

    Setup some simple styling for the form using styled-components.
    Added some state to the Form component.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Form from "./Form";
 
+import AppStyles from "./styles/Form.styles";
+
 function App() {
   return (
-    <>
+    <AppStyles>
       <h1>User Onboarding</h1>
       <Form />
-    </>
+    </AppStyles>
   );
 }
 
