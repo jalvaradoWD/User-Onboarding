e377def94e696e77d2dd2657781078f7c3339aa4
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-08 20:23:46 -0500

    Added some dummy date.
    Added more styling to the projects.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -5,8 +5,27 @@ import Users from "./components/Users";
 
 import AppStyles from "./styles/App.styles";
 
+const dummyData = [
+	{
+		name: "Jose Alvarado",
+		email: "josealvarado1337@gmail.com",
+		password: "123",
+		terms: false,
+		id: "testId",
+		createdAt: "2020-09-08T22:40:13.296Z",
+	},
+	{
+		name: "Jose Alvarado",
+		email: "josealvarado1337@gmail.com",
+		password: "123",
+		terms: false,
+		id: "testId",
+		createdAt: "2020-09-08T22:40:13.296Z",
+	},
+];
+
 function App() {
-	const [users, setUsers] = useState([]);
+	const [users, setUsers] = useState(dummyData);
 
 	return (
 		<AppStyles>
